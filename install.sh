39ba163c65cc2fb65752d7808e40f9487b3b9073
Mahdi Hedhli <mhedhli@gvit.com>
2019-05-03 13:09:49 -0400

    Update install.sh
    Added User input and sed command for setting site URL

--- install.sh
+++ install.sh
@@ -18,5 +18,8 @@ cp c2_cloud/usr/bin/c2_start.sh /usr/bin/c2_start.sh
 cp c2_cloud/etc/systemd/system/hak5c2.service /etc/systemd/system/hak5c2.service
 # Enable Service to run at boot
 systemctl enable hak5c2.service
+echo "What is your site's URL (without http(s)://)?"
+read HOSTNAME
+sed -i "s/YOUR.URL.HERE/${HOSTNAME}/g" /usr/bin/c2_start.sh
 Echo "All set, run the c2_start.sh to get the initial setup token. Going forward you can use the "service hak5c2 start|stop|status" commands.
 
