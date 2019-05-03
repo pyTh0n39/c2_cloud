9298b630499c01b0154d8fb070dde2247612a1b2
Mahdi Hedhli <mhedhli@gvit.com>
2019-05-03 12:23:39 -0400

    Create c2_start.sh
    C2 Start Script for us after initial setup as initial setup requires you run the command to get the output of the token in the terminal for initial login.

---
+++ usr/bin/c2_start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Root Priv Check
+if [ "$(whoami)" != "root" ]; then
+                echo "Sorry, you are not root."
+                        exit 1
+                fi
+
+#Ensure no existing c2 instances are running
+
+killall c2_community-linux-64
+
+#Start Hak5 C2 Community Edition
+
+cd /c2/
+./c2_community-linux-64  -https -hostname c2.cl2apple.com
